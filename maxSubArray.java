564e298a215b5af8f4496d7948657870810cf16f
Tordraki <tordraki@gmail.com>
2022-02-02 09:54:59 -0800

    Problem 4

---
+++ maxSubArray.java
@@ -0,0 +1,26 @@
+public class maxSubArray {
+    public int maxSubArray(int[] nums) {
+        int current = 0;
+        int max = nums[0];
+        for(int i = 0; i<nums.length; i++){
+            if(nums[i]>0){
+                current += nums[i];
+                if(current>max){
+                    max = current;
+                }
+            }
+            else{
+                if(current+nums[i]<=0){
+                    current = 0;
+                    if(max<nums[i]){
+                        max = nums[i];
+                    }
+                }
+                else{
+                    current+=nums[i];
+                }
+            }
+        }
+        return max;
+    }
+}

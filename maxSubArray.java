47665cbbb18b934df4fd32608d861c8c4aec5591
Tordraki <tordraki@gmail.com>
2022-02-04 23:40:10 -0800

    Update maxSubArray.java

--- maxSubArray.java
+++ maxSubArray.java
@@ -9,6 +9,16 @@ public class maxSubArray {
                 if(current>max){
                     max = current;
                 }
+            }else{
+                if(current+nums[i]<=0){
+                    current = 0;
+                    if(max<nums[i]){
+                        max = nums[i];
+                    }
+                }
+                else{
+                    current+=nums[i];
+                }
             }
 
         }

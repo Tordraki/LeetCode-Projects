d13b4bd0a707fd5a90cfad3a70c86cea50d84406
Tordraki <tordraki@gmail.com>
2022-06-16 01:38:19 -0700

    Create lowestCommonAncestor.java

---
+++ lowestCommonAncestor.java
@@ -0,0 +1,39 @@
+public class lowestCommonAncestor {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        return findmin(root, p, q);
+    }
+
+    public TreeNode findmin(TreeNode root, TreeNode p, TreeNode q){
+        if(root.val == p.val || root.val == q.val){
+            return root;
+        }
+        else if(contains(root.right, p)){
+            if(contains(root.right, q)){
+                return findmin(root.right, p, q);
+            }
+            else{
+                return root;
+            }
+        }
+        else if(contains(root.right, q)){
+            return root;
+        }
+        else{
+            return findmin(root.left, p, q);
+        }
+
+
+    }
+
+    public boolean contains(TreeNode root, TreeNode p){
+        if(root == null){
+            return false;
+        }
+        else if(root.val == p.val){
+            return true;
+        }
+        else{
+            return contains(root.left, p) || contains(root.right, p);
+        }
+    }
+}
